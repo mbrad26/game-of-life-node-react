692abfca32ed510f024fd93041343e43d1329207
mbrad26 <mbrad26@gmail.com>
2021-01-04 12:59:14 +0000

    renders with App and root div

---
+++ src/index.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+jest.mock("react-dom", () => ({ render: jest.fn() }));
+
+it("renders with App and root div", () => {
+  const root = document.createElement("div")
+  root.id = "root"
+  document.body.appendChild(root)
+
+  require("./index.js")
+
+  expect(ReactDOM.render).toHaveBeenCalled();
+});
