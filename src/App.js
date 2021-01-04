22668d4c8bd5d2c5dde9cba6242e0dd16c953422
mbrad26 <mbrad26@gmail.com>
2021-01-04 13:54:12 +0000

    renders a table with 25 x 25 cells

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import './App.css'
+
+import './App.css';
 
 const App = () => {
+  const grid = new Array(25).fill(new Array(25).fill(0));
+
   return (
     <div className='app'>
       <h1>Game Of Life</h1>
 
-      <table></table>
+      <table>
+        <tbody>
+          {grid.map((row, i) =>
+            <tr key={`row-${i}`}>
+              {row.map((el, j) => 
+                <td key={j}></td>
+              )}
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
--- src/App.test.js
+++ src/App.test.js
@@ -15,4 +15,9 @@ describe('App', () => {
   it('renders a table', () => {
     expect(screen.getByRole('table')).toBeInTheDocument();
   });
+
+  it('renders a table with 25 x 25 cells', () => {
+    expect(screen.getAllByRole('row').length).toEqual(25);
+    expect(screen.getAllByRole('cell').length).toEqual(625);
+  })
 });
